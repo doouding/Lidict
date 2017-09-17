45dcf80e4be41b908a86c373bdb1abebea0ac219
codertx <codert.sn@gmail.com>
2017-09-17 22:12:52 +0800

    [refactor](authentication): Refactor authentication method

--- jwt_auth/authentication.py
+++ jwt_auth/authentication.py
@@ -2,8 +2,33 @@ from rest_framework import authentication
 from rest_framework import exceptions
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
 from jwt_auth.model import TokenBlackList
+import re
 import jwt
+
+def validate_token(token):
+    """
+    Validate token
+    """
+
+    # check whether the token has been blacklisted
+    try:
+        TokenBlackList.objects.get(token=token)
+        raise exceptions.AuthenticationFailed('invalid token')
+    except ObjectDoesNotExist:
+        pass
+
+    try:
+        user_info = jwt.decode(token, settings.SECRET_KEY)
+        user = User.objects.get(id=user_info['uid'])
+    except jwt.InvalidTokenError as e:
+        if isinstance(e, jwt.ExpiredSignatureError):
+            raise exceptions.AuthenticationFailed('expired token')
+        else:
+            raise exceptions.AuthenticationFailed('invalid token')
+
+    return (user, token)
 
 class JwtAuthentication(authentication.BaseAuthentication):
     """
@@ -11,18 +36,10 @@ class JwtAuthentication(authentication.BaseAuthentication):
     """
 
     def authenticate(self, request):
-        token = request.META.get('auth-token')
+        for path in settings.SKIP_AUTHENTICATION_PATH:
+            match = re.search(path, request.path)
+            if match:
+                return (None, None)
 
-        try:
-            TokenBlackList.objects.get(token=token)
-            raise exceptions.AuthenticationFailed
-        except ObjectDoesNotExist:
-            pass
-
-        try:
-            user_info = jwt.decode(token)
-            user = User.objects.get(id=user_info.id)
-        except jwt.InvalidTokenError:
-            raise exceptions.AuthenticationFailed
-        
-        return (user, None)
+        token = request.META.get('Authorization')
+        return authenticate(token)
