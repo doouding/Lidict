1e0bcdd93c4a9c444c70b105d0222c172c7708a3
codertx <codert.sn@gmail.com>
2017-09-21 00:03:53 +0800

    [refactor]: Refactor url mapping

---
+++ jwt_auth/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+from .views import TokenViews, UserViews
+
+token_create = TokenViews.as_view({
+    'post': 'post'
+})
+
+token_detail = TokenViews.as_view({
+    'delete': 'delete',
+    'get':  'get',
+})
+
+user_create = UserViews.as_view({
+    'post': 'post'
+})
+
+user_detail = UserViews.as_view({
+    'get': 'get'
+})
+
+urlpatterns = [
+    url(r'^auth/?$', token_create),
+    url(r'^auth/(?P<token>.+)/$', token_detail),
+    url(r'^user/?$', user_create),
+    url(r'^user/(?P<username>.+)/$', user_detail)
+]
--- lightil/urls.py
+++ lightil/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import url, include
-from rest_framework import routers
-from jwt_auth.views import TokenCreateViews, TokenVerifyDeleteViews
-
-router = routers.DefaultRouter()
+from jwt_auth.urls import urlpatterns as jwt_auth_patterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/auth/$', TokenCreateViews.as_view()),
-    url(r'^api/auth/(?P<token>.+)/$', TokenVerifyDeleteViews.as_view()),
-    url(r'^api/', include(router.urls, namespace='api'))
+    url(r'^api/', include(jwt_auth_patterns, namespace='api')),
 ]
