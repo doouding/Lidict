264e3032f515d0d3dcc6176d95d9530fb588de29
codertx <codert.sn@gmail.com>
2017-09-21 00:17:01 +0800

    [chore](jwt_auth): Remove old model

--- jwt_auth/model.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class TokenBlackList(models.Model):
-    """
-    Provide deleting token action. Token in the black list is invalid
-    """
-    token = models.CharField(max_length=200)
-    expire = models.DateTimeField()
