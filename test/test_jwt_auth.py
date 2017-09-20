736f782e4cb4b096a4721c42f93f8688264cefed
codertx <codert.sn@gmail.com>
2017-09-20 23:59:15 +0800

    [test](jwt_auth): Add user api test

--- test/test_jwt_auth.py
+++ test/test_jwt_auth.py
@@ -1,11 +1,46 @@
 from django.test import TestCase, Client
-from django.contrib.auth.models import User
+from jwt_auth.models import User 
 import json
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        # used for test unique validation
+        User.objects.create_user(username='codert', password="87654321", email='codert.sn@gmail.com', nickname='哈哈哈')
+
+        self.encoder = json.JSONEncoder()
+   
+    def test_user_create(self):
+        c1 = Client()
+        c2 = Client()
+        c3 = Client()
+        c4 = Client()
+        c5 = Client()
+        c6 = Client()
+        encoder = self.encoder
+
+        # the user info is valid
+        r1 = c1.post('/api/user/', encoder.encode({"username": "newuser1", "password":"12345678", "nickname": "test1", "email": "xx1@example.com"}), content_type="application/json")        
+        # missing required field
+        r2 = c2.post('/api/user/', encoder.encode({"username": "newuser2", "password": "87654321"}), content_type="application/json")
+        # given `username` field is already exist
+        r3 = c3.post('/api/user/', encoder.encode({"username": "codert", "password": "87654321", "nickname": "test3", "email": "xx3@example.com"}), content_type="application/json")
+        # given `email` field is already exist
+        r4 = c4.post('/api/user/', encoder.encode({"username": "newuser4", "password": "87654321", "nickname": "test4", "email": "codert.sn@gmail.com"}), content_type="application/json")
+        # given `username` field is already exist but uppercase
+        r5 = c5.post('/api/user/', encoder.encode({"username": "CODERT", "password": "87654321", "nickname": "test5", "email": "xx5@example.com"}), content_type="application/json")
+        # given `email` field is already exist bu uppercase
+        r6 = c6.post('/api/user/', encoder.encode({"username": "newuser6", "password": "87654321", "nickname": "test6", "email": "CODERT.SN@gmail.com"}), content_type="application/json")
+
+        self.assertEqual(201, r1.status_code)
+        self.assertEqual(400, r2.status_code)
+        self.assertEqual(400, r3.status_code)
+        self.assertEqual(400, r4.status_code)
+        self.assertEqual(400, r5.status_code)
+        self.assertEqual(400, r6.status_code)
 
 class JwtAuthTestCase(TestCase):
     def setUp(self):
-        u = User.objects.create_user(username="codert", password="87654321")
-        u.save()
+        User.objects.create_user(username="codert", password="87654321", email="xxx@xx.com", nickname="xx")
 
         self.encoder = json.JSONEncoder()
 
@@ -16,9 +51,13 @@ class JwtAuthTestCase(TestCase):
         c4 = Client()
         encoder = self.encoder
 
+        # the user info is valid
         r1 = c1.post('/api/auth/', encoder.encode({ "username": "codert", "pwd": "87654321" }), content_type="application/json")
+        # missing `pwd` field
         r2 = c2.post('/api/auth/', encoder.encode({ "username": "codert", "xx": "xxxxxxxx" }), content_type="application/json")
+        # the password is incorrect
         r3 = c3.post('/api/auth/', encoder.encode({ "username": "codert", "pwd": "12345678" }), content_type="application/json")
+        # the user isn't exsit
         r4 = c4.post('/api/auth/', encoder.encode({ "username": "donotexist", "pwd": "12345678" }), content_type="application/json")
 
         self.assertEqual(201, r1.status_code)
