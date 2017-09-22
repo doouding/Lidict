1b8355db917a65b45a2155b07e14faea99ef575a
codertx <codert.sn@gmail.com>
2017-09-23 01:28:42 +0800

    [chore](setting): Update test setting

--- lightil/settings/test.py
+++ lightil/settings/test.py
@@ -17,3 +17,19 @@ DATABASES = {
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8$zd&J52iqt;w>pDE.UYS)osUM0A,W{jpBMU71mLz3Q4$!{Xn?'
+
+# Some django app haven't complete
+# thus add it to INSTALLED_APPS may cause
+# ci build fail
+
+# Remove it once all django app is done
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'jwt_auth.apps.JwtAuthConfig',
+    'rest_framework'
+]
