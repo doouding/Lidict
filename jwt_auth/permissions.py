5e45d755c63a9ea5537d8dacdbb42e78bb274a98
codertx <codert.sn@gmail.com>
2017-09-23 01:08:31 +0800

    [contrib](jwt_auth): Add custom permission

--- jwt_auth/permissions.py
+++ jwt_auth/permissions.py
@@ -1,12 +1,29 @@
+"""
+Custom permissions
+"""
 from rest_framework import permissions
 
-class IsOwnerOrCreate(permissions.BasePermission):
+# Note that the `has_object_permission` will not run when do
+# `create` (which is `POST` method) action
+
+# These's no need to consider the unauthenticated request because
+# they have been block by authentication scheme except those in
+# AUTHENTICATION_EXCLUDE settings
+
+class IsOwner(permissions.BasePermission):
     """
     Check whether the resource's owner is authenticated user
     or creating new resource which require no authentication info
     """
     def has_object_permission(self, request, view, obj):
-        if request.method == 'POST':
-            return True
-        else:
-            return obj.owner == request.user
+        return obj.owner == request.user
+
+class IsUser(permissions.BasePermission):
+    """
+    This permission is used when a request want to retrieve or update
+    a user's info.
+    The authenticated user must be exactly the same with the user
+    which the request want to do something with.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
